60c0e24c99b9eb29e820f282b8f4b727d019d156
sean0921 <sean.li.shin.ho@gmail.com>
2019-05-18 15:44:12 +0800

    simplfied first example

--- pygitest/01_blank_window.py
+++ pygitest/01_blank_window.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
 ## Empty Window
 #### show a empty window
+
 import gi
 
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-def main():
-    win = Gtk.Window()
-    win.connect("delete-event", Gtk.main_quit)
-    win.show_all()
-    Gtk.main()
-
-if __name__ == "__main__":
-    main()
+win = Gtk.Window()
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+Gtk.main()
